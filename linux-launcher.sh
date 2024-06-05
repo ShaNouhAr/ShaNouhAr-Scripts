5b031136a506a2017b8fcf7a6efcb5210685a7af
ShaNouhAr <115699487+ShaNouhAr@users.noreply.github.com>
2024-06-05 16:04:07 +0200

    Update linux-launcher.sh

--- linux-launcher.sh
+++ linux-launcher.sh
@@ -20,12 +20,12 @@ get_github_content() {
     curl -s -H "User-Agent: shell-script" "$1"
 }
 
-# Fonction pour afficher le contenu d'un dossier
+# Fonction pour afficher la liste des scripts disponibles
 show_directory_content() {
     ITEMS=$(get_github_content "$1")
     COUNTER=1
     echo -e "${COLOR_YELLOW}0. .. (Revenir en arrière)${COLOR_RESET}"
-    echo "$ITEMS" | jq -r '.[] | "\(.type) \(.name) \(.path) \(.url)"' | while read -r TYPE NAME PATH URL; do
+    echo "$ITEMS" | jq -r '.[] | "\(.type) \(.name)"' | while read -r TYPE NAME; do
         if [ "$TYPE" == "dir" ]; then
             echo -e "${COLOR_CYAN}$COUNTER. $NAME (Dossier)${COLOR_RESET}"
         else
@@ -35,27 +35,14 @@ show_directory_content() {
     done
 }
 
-# Fonction pour exécuter un script localement
+# Fonction pour exécuter un script
 execute_script() {
-    SCRIPT_URL=$1
-    FILE_NAME=$2
+    SCRIPT_URL="$1"
+    FILE_NAME=$(basename "$2")
     TEMP_SCRIPT_PATH="/tmp/$FILE_NAME"
     curl -s -L -o "$TEMP_SCRIPT_PATH" "$SCRIPT_URL"
     chmod +x "$TEMP_SCRIPT_PATH"
-
-    EXTENSION="${FILE_NAME##*.}"
-
-    case "$EXTENSION" in
-        sh)
-            bash "$TEMP_SCRIPT_PATH"
-            ;;
-        py)
-            python3 "$TEMP_SCRIPT_PATH"
-            ;;
-        *)
-            echo -e "${COLOR_RED}Type de fichier non supporté: $EXTENSION${COLOR_RESET}"
-            ;;
-    esac
+    bash "$TEMP_SCRIPT_PATH"
 }
 
 # Boucle principale de navigation
@@ -64,12 +51,9 @@ while [ "$EXIT" = false ]; do
     echo -e "${COLOR_RED}=============================================="
     echo -e "                 ShaNouhAr-Scripts             "
     echo -e "==============================================${COLOR_RESET}"
-
     CONTENT=$(get_github_content "$CURRENT_URL")
     show_directory_content "$CURRENT_URL"
-
-    read -p "Entrez un numéro pour naviguer ou exécuter un script, ou 'exit' pour quitter: " INPUT
-
+    read -p "Entrez le numéro du script à exécuter ou 'exit' pour quitter: " INPUT
     if [ "$INPUT" == "exit" ]; then
         EXIT=true
     elif [ "$INPUT" -eq 0 ]; then
@@ -84,9 +68,7 @@ while [ "$EXIT" = false ]; do
         SELECTED_ITEM=$(echo "$CONTENT" | jq -r ".[$((INPUT-1))]")
         SELECTED_TYPE=$(echo "$SELECTED_ITEM" | jq -r '.type')
         SELECTED_NAME=$(echo "$SELECTED_ITEM" | jq -r '.name')
-        SELECTED_PATH=$(echo "$SELECTED_ITEM" | jq -r '.path')
         SELECTED_URL=$(echo "$SELECTED_ITEM" | jq -r '.url')
-
         if [ "$SELECTED_TYPE" == "dir" ]; then
             PARENT_URLS+=("$CURRENT_URL")
             CURRENT_URL="$SELECTED_URL"

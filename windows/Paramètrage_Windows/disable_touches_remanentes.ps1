0ed9b9a7c2663815b25a4ecbbaec503fd1d6c777
ShaNouhAr <115699487+ShaNouhAr@users.noreply.github.com>
2024-06-06 16:46:23 +0200

    Update disable_touches_remanentes.ps1

--- windows/Paramètrage_Windows/disable_touches_remanentes.ps1
+++ windows/Paramètrage_Windows/disable_touches_remanentes.ps1
@@ -1,14 +1,19 @@
-# Désactiver les touches rémanentes
+# Désactiver les touches rémanentes pour l'utilisateur actuel
 Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\StickyKeys' -Name "Flags" -Value 506
 Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\Keyboard Response' -Name "Flags" -Value 122
 Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\ToggleKeys' -Name "Flags" -Value 58
 Set-ItemProperty -Path 'HKCU:\Control Panel\Accessibility\SoundSentry' -Name "Flags" -Value 58
 
-# Désactiver les touches rémanentes au niveau du système
-Set-ItemProperty -Path 'HKU\.DEFAULT\Control Panel\Accessibility\StickyKeys' -Name "Flags" -Value 506
-Set-ItemProperty -Path 'HKU\.DEFAULT\Control Panel\Accessibility\Keyboard Response' -Name "Flags" -Value 122
-Set-ItemProperty -Path 'HKU\.DEFAULT\Control Panel\Accessibility\ToggleKeys' -Name "Flags" -Value 58
-Set-ItemProperty -Path 'HKU\.DEFAULT\Control Panel\Accessibility\SoundSentry' -Name "Flags" -Value 58
+# Désactiver les touches rémanentes pour le profil utilisateur par défaut
+Set-ItemProperty -Path 'HKU:S-1-5-19\Control Panel\Accessibility\StickyKeys' -Name "Flags" -Value 506
+Set-ItemProperty -Path 'HKU:S-1-5-19\Control Panel\Accessibility\Keyboard Response' -Name "Flags" -Value 122
+Set-ItemProperty -Path 'HKU:S-1-5-19\Control Panel\Accessibility\ToggleKeys' -Name "Flags" -Value 58
+Set-ItemProperty -Path 'HKU:S-1-5-19\Control Panel\Accessibility\SoundSentry' -Name "Flags" -Value 58
+
+Set-ItemProperty -Path 'HKU:S-1-5-20\Control Panel\Accessibility\StickyKeys' -Name "Flags" -Value 506
+Set-ItemProperty -Path 'HKU:S-1-5-20\Control Panel\Accessibility\Keyboard Response' -Name "Flags" -Value 122
+Set-ItemProperty -Path 'HKU:S-1-5-20\Control Panel\Accessibility\ToggleKeys' -Name "Flags" -Value 58
+Set-ItemProperty -Path 'HKU:S-1-5-20\Control Panel\Accessibility\SoundSentry' -Name "Flags" -Value 58
 
 # Informer l'utilisateur que l'opération est terminée
 Write-Output "Les touches rémanentes ont été désactivées avec succès."
